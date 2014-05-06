73f6ee57c38dd39f2949f3794321bc6d6e615252
Vladislav Kraidiky Goloshchapov <Vladislav Goloshchapov v.goloshchapov@pixonic.ru>
2014-05-06 17:17:27 +0400

    Переловил в сцене Кубики. Три строчки но со всеми отвлечениями и настройками потратил битый час.

---
+++ Assets/Core/CubesOrderer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+// Скрипт навешиваем на камеру. Логично, ей же нужно выстраивать всё относительно камеры.
+public class CubesOrderer : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private List<CubeContainer> containers = new List<CubeContainer> ();
+
+	public void InspectAndDecorateCubes () {
+		// В сцене найти кубики, и проверить не были ли они у нас уже раньше.
+		var newContainers = GameObject.FindObjectsOfType<BoxCollider>().Cast<BoxCollider>().SkipWhile(
+				box => containers.Any (container => container.Box == box)
+			).Select(box => new CubeContainer(){Box = box});
+		// Всё, новые элементы нашли, теперь пора с ними что-нибудь делать.
+
+			
+	}
+	public void CreateRandomBox() {
+		GameObject = new GameObject();
+
+
+	}
+
+	void OnGUI ()
+	{
+			//GUI.Label (new Rect (10, 60, 100, 50), "Scores: ");
+			if (GUI.Button (new Rect (10, 10, 250, 24), "Создать новый случайный кубик")) {
+					// TODO
+			}
+			if (GUI.Button (new Rect (10, 40, 200, 24), "Поискать новые кубики")) {
+					InspectAndDecorateCubes ();
+			}
+	}
+}
+
+struct CubeContainer
+{
+	public GameObject Target;
+	public BoxCollider Box;
+	public Vector3 SelectedPosition;
+}
---
+++ Assets/Core/Tools.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static class Tools {
+    /// <summary>Вызвать делегата в отношении каждого экземпляра в последовательности любого типа.</summary>
+    /// <param name="source">Перечислитель с исходными данными.</param>
+    /// <param name="func">Делегат, который надо вызывать.</param>
+    public static void Each<SequenceType>(this IEnumerator<SequenceType> source, Action<SequenceType> func) {
+        while (source.MoveNext())
+            func(source.Current);
+    }
+    /// <summary>Вызвать делегата в отношении каждого экземпляра в последовательности любого типа и передать ему индекс.</summary>
+    /// <param name="source">Перечислитель с исходными данными.</param>
+    /// <param name="func">Делегат, который надо вызывать. Второй параметр передаваемый делегату - индекс в последовательности. Отсчёт начинается со следующего элемента последовательности. В нетронутой - с начала.</param>
+    public static void Each<SequenceType>(this IEnumerator<SequenceType> source, Action<SequenceType, int> func) {
+        for (int i = 0; source.MoveNext(); i++)
+            func(source.Current, i);
+    }
+    /// <summary>Вызвать делегата в отношении каждого экземпляра в последовательности любого типа.</summary>
+    /// <param name="source">Перечислитель с исходными данными.</param>
+    /// <param name="func">Делегат, который надо вызывать.</param>
+    public static void Each<SequenceType>(this IEnumerable<SequenceType> source, Action<SequenceType> func) {
+        source.GetEnumerator().Each(func);
+    }
+    /// <summary>Вызвать делегата в отношении каждого экземпляра в последовательности любого типа и передать ему индекс.</summary>
+    /// <param name="source">Перечислитель с исходными данными.</param>
+    /// <param name="func">Делегат, который надо вызывать. Второй параметр передаваемый делегату - индекс в последовательности. Отсчёт начинается со следующего элемента последовательности. В нетронутой - с начала.</param>
+    public static void Each<SequenceType>(this IEnumerable<SequenceType> source, Action<SequenceType, int> func) {
+        source.GetEnumerator().Each(func);
+    }
+    /// <summary>Приводит элементы последовательности к строке и соединяет эти строки. Лучше Aggregate потому что не падает при пустом списке</summary>
+    /// <param name="source">Исходная последовательность любого типа.</param>
+    /// <param name="separator">Строка разделитель, вставляемая между элементами. Чаще всего "," или "\n"</param>
+    public static string ToJoinedString<SequenceType>(this IEnumerator<SequenceType> source, string separator) {
+        string msg = "";
+        while (source.MoveNext()) {
+            msg += (msg == "" ? "" : separator) + source.Current.ToString();
+        }
+        return msg;
+    }
+    /// <summary>Приводит элементы последовательности к строке и соединяет эти строки. Лучше Aggregate потому что не падает при пустом списке</summary>
+    /// <param name="source">Исходная последовательность любого типа.</param>
+    /// <param name="separator">Строка разделитель, вставляемая между элементами. Чаще всего "," или "\n"</param>
+    public static string ToJoinedString<SequenceType>(this IEnumerable<SequenceType> source, string separator) {
+        return source.GetEnumerator().ToJoinedString(separator);
+    }
+}
